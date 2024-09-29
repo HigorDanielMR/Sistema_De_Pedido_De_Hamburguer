c45ba15c4c5cbb92b0e5e05891d57d37f73cbbf5
Higor Daniel <higordanielmorais@gmail.com>
2024-09-29 18:20:55 -0300

    feat: Finalizando o cabecalho e o rodape

--- 1_sistema_de_pedido_de_hamburguer/src/App.vue
+++ 1_sistema_de_pedido_de_hamburguer/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar/>
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <Footer/>
 </template>
@@ -12,10 +12,33 @@ import Navbar from './components/Navbar.vue';
     components:{
       Navbar,
       Footer
+    },
+    data(){
+      return{
+        logo_src: "/img/logo.png",
+        app_name: "Make Your Burger"
+      }
     }
   }
 </script>
 
 <style>
+ * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container{
+    margin: 50px;
+    min-height: 576px;
+  }
+
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
