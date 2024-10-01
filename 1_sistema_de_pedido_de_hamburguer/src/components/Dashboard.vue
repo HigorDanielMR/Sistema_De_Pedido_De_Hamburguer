8dbad64bc6e33b2449928356a6779955c888ded6
Higor Daniel <higordanielmorais@gmail.com>
2024-09-30 21:12:39 -0300

    feat: Imlementando metodo get para obter os status do banco

--- 1_sistema_de_pedido_de_hamburguer/src/components/Dashboard.vue
+++ 1_sistema_de_pedido_de_hamburguer/src/components/Dashboard.vue
@@ -24,6 +24,7 @@
                 <div>
                     <select name="status" class="status">
                         <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">{{s.tipo}}</option>
                     </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>
@@ -50,7 +51,14 @@
 
                 this.burgers = data;
 
-                //resgatar os status
+                this.obterStatus();
+            },
+            async obterStatus(){
+                const req = await fetch("http://localhost:3000/status");
+
+                const data = await req.json();
+
+                this.status = data;
             }
         },
         mounted(){
